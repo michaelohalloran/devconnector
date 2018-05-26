03886eac14815e31a3bc8b0d10b95d6a8d46bf7d
MJ <kipsang20323@gmail.com>
2018-05-26 01:08:42 -0600

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://bison:bison45@ds235860.mlab.com:35860/devconnector'
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//ROUTES
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+
+const app = express();
+
+//Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//connect to Mongoose
+mongoose
+    .connect(db)
+    .then(()=> console.log('MongoDB connected'))
+    .catch((err)=>console.log(err));
+
+app.get('/', (req, res)=> {
+    res.send('Hi');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> {
+    console.log(`server running on port ${port}`);
+});
